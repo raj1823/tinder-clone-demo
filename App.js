724cc7cea45db318800aa6719647d8ef32a53955
Raj Gupta <rajgupta@Rajs-MacBook-Pro.local>
2021-12-15 14:42:53 +0530

    cleaning

--- App.js
+++ App.js
@@ -9,17 +9,6 @@ import {
   PanResponder,
 } from 'react-native';
 
-const SCREEN_HEIGHT = Dimensions.get('window').height;
-const SCREEN_WIDTH = Dimensions.get('window').width;
-import Icon from 'react-native-vector-icons/Ionicons';
-const Users = [
-  {id: '1', uri: require('./assets/1.jpg')},
-  {id: '2', uri: require('./assets/2.jpg')},
-  {id: '3', uri: require('./assets/3.jpg')},
-  {id: '4', uri: require('./assets/4.jpg')},
-  {id: '5', uri: require('./assets/5.jpg')},
-];
-
 export default class App extends React.Component {
   constructor(props) {
     super(props);
